9a302e11b9eb11b45a7df5380786b2902f96aed4
Art Tay <tay.art.42@gmail.com>
2021-12-23 20:14:35 -0600

    Changed declarations with '=' to '<-'

--- SP_500_Option_Pull.r
+++ SP_500_Option_Pull.r
@@ -5,12 +5,12 @@ library(quantmod)
 library(tidyverse)
 
 # Import helper functions. 
-getOptionData = function (symbol = "F", exp, src = "yahoo", type)
+getOptionData <- function(symbol = "F", exp, src = "yahoo", type)
 {
-    opt_Chain = getOptionChain(Symbols = symbol, Exp = exp, src = src)
-    opt_Chain_return = list()
+    opt_Chain <- getOptionChain(Symbols = symbol, Exp = exp, src = src)
+    opt_Chain_return <- list()
     for(name in names(opt_Chain)){
-      opt_Chain_return[[name]] = opt_Chain %>% 
+      opt_Chain_return[[name]] <- opt_Chain %>% 
         pluck(name) %>% 
         pluck(type) %>% 
         select(Strike, Bid, Ask, Last, Vol) %>%
@@ -18,10 +18,10 @@ getOptionData = function (symbol = "F", exp, src = "yahoo", type)
   } 
   #Data Formating
   #Adding DTE 
-    opt_Chain_return = bind_rows(opt_Chain_return,.id = "Exp")
-    opt_Chain_return$Exp = as.Date(opt_Chain_return$Exp, format = "%b.%d.%Y")
-    opt_Chain_return$Strike = ordered(as.factor(opt_Chain_return$Strike))
-    opt_Chain_return$DTE = (opt_Chain_return$Exp - Sys.Date())
+    opt_Chain_return <- bind_rows(opt_Chain_return, .id = "Exp")
+    opt_Chain_return$Exp <- as.Date(opt_Chain_return$Exp, format = "%b.%d.%Y")
+    opt_Chain_return$Strike <- ordered(as.factor(opt_Chain_return$Strike))
+    opt_Chain_return$DTE <- (opt_Chain_return$Exp - Sys.Date())
     return(opt_Chain_return)
 }
 
