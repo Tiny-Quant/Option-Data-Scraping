d31d0d3e754a5d1698b787cae14d47228e50f297
Art Tay <tay.art.42@gmail.com>
2021-12-24 20:48:28 -0600

    Insignificant changes

--- SP_500_Option_Pull_Inital.r
+++ SP_500_Option_Pull_Inital.r
@@ -1,6 +1,6 @@
 
 # To track execution timestart_time <- Sys.time()
-start_time = Sys.time()
+start_time <- Sys.time()
 # Pull option data from all the stocks in the S&P 500.
 # Stores the data as multiple csv files.
 
@@ -10,24 +10,24 @@ library(tidyverse)
 # Import helper functions.
 getOptionData <- function(symbol = "F", exp, src = "yahoo", type)
 {
-  opt_Chain <- getOptionChain(Symbols = symbol, Exp = exp, src = src)
-  opt_Chain_return <- list()
-  for(name in names(opt_Chain)) {
-    opt_Chain_return[[name]] <- opt_Chain %>%
-      pluck(name) %>%
-      pluck(type) %>%
-      filter(!is.na(Vol) & (Last + Bid + Ask) > 0.03) %>%
-      select(-c(Chg, ITM))
-  }
-  #Data Formating
-  opt_Chain_return <- bind_rows(opt_Chain_return, .id = "Exp")
-  opt_Chain_return %>% mutate(
-    Exp = as.Date(Exp, format = "%b.%d.%Y"),
-    Strike = ordered(as.factor(Strike)),
-    Underlying = getQuote(Symbols = symbol)$Last,
-    DTE = Exp - Sys.Date(),
-    )
-  return(opt_Chain_return)
+    opt_Chain <- getOptionChain(Symbols = symbol, Exp = exp, src = src)
+    opt_Chain_return <- list()
+    for(name in names(opt_Chain)) {
+        opt_Chain_return[[name]] <- opt_Chain %>%
+            pluck(name) %>%
+            pluck(type) %>%
+            filter(!is.na(Vol) & (Last + Bid + Ask) > 0.03) %>%
+            select(-c(Chg, ITM))
+    }
+    #Data Formating
+    opt_Chain_return <- bind_rows(opt_Chain_return, .id = "Exp")
+    opt_Chain_return <- opt_Chain_return %>% mutate(
+        Exp = as.Date(Exp, format = "%b.%d.%Y"),
+        Strike = ordered(as.factor(Strike)),
+        Time_Pull = Sys.time(),
+        Underlying = getQuote(Symbols = symbol)$Last,
+        DTE = difftime(Exp, Sys.Date(), unit = "days"))
+    return(opt_Chain_return)
 }
 
 # Declare a file path were the data should be stored into
@@ -37,7 +37,7 @@ SP_500_Symbols <- read.csv(file = paste("C:/Users/Admin/Google Drive/",
 names(SP_500_Symbols) <- "Symbol"
 
 ## Test ##
-SP_500_Symbols = c("AAPL", "F", "GE", "AMZN")
+SP_500_Symbols <- c("AAPL", "F", "GE", "AMZN")
 
 # Calls
 # First run to create the csv files.
@@ -46,11 +46,10 @@ setwd(paste("C:/Users/Public/Data Mining/",
   sep = ""))
 
 for(symbol in SP_500_Symbols){
-  x <- getOptionData(symbol = symbol,
-    exp = c("2021", "2022", "2023", "2024"),
-    type = "calls") %>%
-        mutate(Time_Pull = Sys.time())
-  write.csv(x, file = paste(symbol, "-Calls.csv", sep = ""))
+    x <- getOptionData(symbol = symbol,
+        exp = c("2021", "2022", "2023", "2024"),
+        type = "calls")
+    write.csv(x, file = paste(symbol, "-Calls.csv", sep = ""))
 }
 
 #Puts
@@ -60,11 +59,10 @@ setwd(paste("C:/Users/Public/Data Mining/",
   sep = ""))
 
 for(symbol in SP_500_Symbols){
-  x <- getOptionData(symbol = symbol,
-    exp = c("2021", "2022", "2023", "2024"),
-    type = "puts") %>%
-        mutate(Time_Pull = Sys.time())
-  write.csv(x, file = paste(symbol, "-Puts.csv", sep = ""))
+    x <- getOptionData(symbol = symbol,
+        exp = c("2021", "2022", "2023", "2024"),
+        type = "puts")
+    write.csv(x, file = paste(symbol, "-Puts.csv", sep = ""))
 }
 # To track execution time.
 end_time <- Sys.time()
@@ -73,6 +71,6 @@ end_time <- Sys.time()
 df_ex_time <- cbind(end_time - start_time, toString(Sys.time()))
 
 write.table(df_ex_time,
-   file = paste("C:/Users/Public/Data Mining/Option Data/",
+    file = paste("C:/Users/Public/Data Mining/Option Data/",
     "Historical Quotes - Yahoo/SP_500/Execution Times.csv", sep = ""),
-   append = T, sep = ",", quote = F, col.names = F, row.names = F)
+    append = T, sep = ",", quote = F, col.names = F, row.names = F)
